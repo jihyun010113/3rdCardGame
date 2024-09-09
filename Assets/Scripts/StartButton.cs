744fe38d5fa798610c9121690b9cee7ab5963e6b
gogipie <sjh010113@gmail.com>
2024-09-09 20:09:35 +0900

    [Feature] StartScene

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+
+
+    public void GameStart()
+    {
+
+
+        SceneManager.LoadScene("MainScene");
+
+
+
+    }
+
+
+}
