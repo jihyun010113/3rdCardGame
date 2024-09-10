fe4a26f99058b7e5b42c0c6d6dd83d5a34fc44f5
gogipie <sjh010113@gmail.com>
2024-09-10 19:28:59 +0900

    Revert "Merge branch 'Dev' into feature_IH"
    This reverts commit 195617ce707ca3e52c3728d75251e50aaf3b2a1f, reversing
    changes made to afd8db98364ddebfaf182258a61b8e5c2cf460fa.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.SocialPlatforms.Impl;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    public LevelManager levelManager;
 
     public float time;
     float time_Tmp;
@@ -116,7 +117,7 @@ public class GameManager : MonoBehaviour
 
     public void Correct_Card()
     {
-        LevelManager.Instance.Match_CntUp();
+        levelManager.Match_CntUp();
         if (time_Tmp + 3f <= time)
             time_Tmp += 3f;
         else
