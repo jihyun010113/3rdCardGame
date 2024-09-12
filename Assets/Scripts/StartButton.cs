f1fa7ee3599771400baf8cb44275eabf933ecea8
HyunSeok Suh <suhya990312@gmail.com>
2024-09-12 20:20:07 +0900

    Fix

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -9,8 +9,7 @@ using UnityEngine.UI;
 public class StartButton : MonoBehaviour
 {
     public GameObject selectSence;
-    bool isHard;
-    float goalScore;
+    float goalScore = 30f;
     public Button hardBtn;
     public Text BestScoreNow_Txt;
     float bestNow;
@@ -21,70 +20,33 @@ public class StartButton : MonoBehaviour
     public void Start()
       
     {
-       //PlayerPrefs.DeleteAll(); // 베스트 스코어 초기화
+        //PlayerPrefs.DeleteAll(); // 베스트 스코어 초기화
         Time.timeScale = 1; // 타임스케일 초기화
         
 
         hardBtn.interactable = false;
-        
-        
-
-
-
-
-
-        if (PlayerPrefs.HasKey("best"))
-        {
-
-           bestNow = PlayerPrefs.GetFloat("best");
-           BestScoreNow_Txt.text = bestNow.ToString("N1");
-
-        }
-
-        else
-        {
-
-            BestScoreNow_Txt.text = "점수 없음";
-        }
-
-
-
     }
 
-    public void Update()
-    {
-        goalScore = 30.0f;
-        isHard = goalScore <= bestNow;
-
-
-        if ( isHard )
-        {
-
-            
-            hardBtn.interactable = true;
-
-
-        }
-
-
-
-    }
 
     public void StageSelect()
     {
+        goalScore = 30f;
 
-
-        if ( !selectSence.activeSelf )
+        if (PlayerPrefs.HasKey("best"))
         {
-
-           
-            selectSence.SetActive(true);
-
-
-
+            bestNow = PlayerPrefs.GetFloat("best");
+            BestScoreNow_Txt.text = bestNow.ToString("N1");
+        }
+        else
+        {
+            bestNow = 0;
+            BestScoreNow_Txt.text = "점수 없음";
         }
 
+        if (bestNow >= goalScore)
+            hardBtn.interactable = true;
 
+        selectSence.SetActive(true);
 
     }
 
@@ -92,8 +54,8 @@ public class StartButton : MonoBehaviour
     {
 
 
-        SceneManager.LoadScene(0);
-        AudioManager.Instance.ChangeBGM(0);
+        SceneManager.LoadScene("StartScene");
+
 
     }
 
@@ -101,8 +63,8 @@ public class StartButton : MonoBehaviour
     {
 
 
-        SceneManager.LoadScene(3);
-        AudioManager.Instance.ChangeBGM(3);
+        SceneManager.LoadScene("EazyScene");
+
 
     }
 
@@ -110,8 +72,8 @@ public class StartButton : MonoBehaviour
 
     {
 
-        SceneManager.LoadScene(2);
-        AudioManager.Instance.ChangeBGM(2);
+        SceneManager.LoadScene("NormalScene");
+
 
 
     }
@@ -120,8 +82,8 @@ public class StartButton : MonoBehaviour
 
     {
 
-            SceneManager.LoadScene(1);
-        AudioManager.Instance.ChangeBGM(1);
+            SceneManager.LoadScene("HardScene");
+
 
     }
 
