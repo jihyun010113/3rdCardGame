d2a36fd350b6a84b8d1500bdd9faf2e1a5b87bee
gogipie <sjh010113@gmail.com>
2024-09-12 10:32:57 +0900

    Update Retry.cs

--- Assets/Scripts/Retry.cs
+++ Assets/Scripts/Retry.cs
@@ -5,37 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class Retry : MonoBehaviour
 {
-
-
     public void ReMain()
     {
-
         SceneManager.LoadScene("StartScene");
-
     }
-
     public void ReEasy()
     {
-
         SceneManager.LoadScene("EazyScene");
-
     }
-
     public void ReNormal()
 
     {
-
         SceneManager.LoadScene("NormalScene");
-
     }
-
     public void ReHard()
 
     {
-
         SceneManager.LoadScene("HardScene");
-
     }
-
-
 }
