9b2633146875825a66db53c9f40a14c031f99703
gogipie <sjh010113@gmail.com>
2024-09-12 10:32:16 +0900

    Update CreditButton.cs

--- Assets/Scripts/CreditButton.cs
+++ Assets/Scripts/CreditButton.cs
@@ -11,92 +11,45 @@ public class CreditButton : MonoBehaviour
 
     /*private void Update()
     {
-
         if (Input.anyKeyDown) {
-
-
-
             if(CreditSence.activeSelf == true)
             {
-
-
                 CreditSence.SetActive(false);
                 Debug.Log("if¹® ½ÇÇàµÊ");
-
             }
-
-
-
-
         }
-
         if (Input.GetKeyDown(KeyCode.Escape)  )
         {
-
-
             OptionSence.SetActive(false);
-
-
         }
-
-
-
-
     }*/
-
     public void Option()
     {
-
-
         if (OptionSence.activeSelf == false)
         {
-
             OptionSence.SetActive(true);
-
-
         }
-
     }
 
     public void OptionBack()
     {
-
         OptionSence.SetActive(false);
-
-
     }
 
     public void Credit()
     {
-
-
         if (CreditSence.activeSelf == false)
         {
-
-
             CreditSence.SetActive(true);
-
         }
-
-
     }
 
     public void CreditBack()
     {
-
-
-
         if (CreditSence.activeSelf == true)
         {
-
-
             CreditSence.SetActive(false);
-
-
-
         }
-
     }
-
 }
 
